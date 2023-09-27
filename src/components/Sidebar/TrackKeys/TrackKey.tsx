4256a0202c9f1e6bb287ab8ad58c9f407de218b6
Robin Blixter <robin.blixter@intersport.se>
2023-09-27 11:03:02 +0200

    feat: added button for expanding tracks

--- src/components/Sidebar/TrackKeys/TrackKey.tsx
+++ src/components/Sidebar/TrackKeys/TrackKey.tsx
@@ -21,10 +21,21 @@ const DEFAULT_PROPS: DefaultProperties<Props> = {
 export default function TrackKey(props: Props): JSX.Element {
   const { track, toggleOpen } = setDefaultProperties(props, DEFAULT_PROPS);
   const { title, tracks, isOpen, sideComponent } = track;
+  const isExpandable = isOpen !== undefined;
 
   return (
     <li className="rt-track-key">
       <div className="rt-track-key__entry">
+      {isExpandable && (
+          <button
+            title="Expand track"
+            className={`rt-track-key__toggle ${isOpen ? 'rt-track-key__toggle--close' : 'rt-track-key__toggle--open'}`}
+            onClick={() => toggleOpen(track)}
+            type="button"
+          >
+            {isOpen ? 'Close' : 'Open'}
+          </button>
+        )}
         <span className="rt-track-key__title">{title}</span>
         {sideComponent ? cloneElement(sideComponent) : null}
       </div>
